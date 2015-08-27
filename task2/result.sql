1b1ca520ffe7c9b6fb5fa73e992140ea70921c5e
Michael Akimov <michael@island-future.ru>
2015-08-27 09:15:59 +0300

    solve task2, variant2 (9 min)

--- task2/result.sql
+++ task2/result.sql
@@ -34,7 +34,7 @@ ALTER TABLE `BooksAuthors1` ADD CONSTRAINT `fk_BooksAuthors1_id_Authors1` FOREIG
 -- ------------
 -- Вариант 2
 -- Храним idшники в полях idsBooks, idsAuthors - через запятую, а поиск осуществляем через SOLR, при загрузку данных в него, указываем, что это мультиполя
-CREATE TABLE `Authors1` (
+CREATE TABLE `Authors2` (
   `id` SERIAL,
   `sLastName` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `sName` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE `Authors1` (
   PRIMARY KEY  (`id`)
 );
 
-CREATE TABLE `Books1` (
+CREATE TABLE `Books2` (
   `id` SERIAL,
   `sName` VARCHAR(512) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `idsAuthors` VARCHAR(128) CHARACTER SET ascii COLLATE ascii_bin NOT NULL DEFAULT '',
