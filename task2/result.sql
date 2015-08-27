356a9b148504e8c4322a933dcfeeffdf836db716
Michael Akimov <michael@island-future.ru>
2015-08-27 09:15:28 +0300

    solve task2, varian2 (8 min)

--- task2/result.sql
+++ task2/result.sql
@@ -30,3 +30,24 @@ ALTER TABLE `BooksAuthors1` ADD CONSTRAINT `fk_BooksAuthors1_id_Authors1` FOREIG
     REFERENCES `Authors1` (`id`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT;
+
+-- ------------
+-- Вариант 2
+-- Храним idшники в полях idsBooks, idsAuthors - через запятую, а поиск осуществляем через SOLR, при загрузку данных в него, указываем, что это мультиполя
+CREATE TABLE `Authors1` (
+  `id` SERIAL,
+  `sLastName` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sName` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sSecondName` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `idsBooks` VARCHAR(256) CHARACTER SET ascii COLLATE ascii_bin NOT NULL DEFAULT '',
+  PRIMARY KEY  (`id`)
+);
+
+CREATE TABLE `Books1` (
+  `id` SERIAL,
+  `sName` VARCHAR(512) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `idsAuthors` VARCHAR(128) CHARACTER SET ascii COLLATE ascii_bin NOT NULL DEFAULT '',
+  `cacheAuthors` VARCHAR(512) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY  (`id`)
+);
+
